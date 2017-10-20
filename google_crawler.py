7335968ff3f05d1074238edff8cc7601ab6f800f
林鴻 <linhung@linhongde-Mac-mini.local>
2017-10-20 16:00:37 +0800

    Add more p_key and n_key

--- google_crawler.py
+++ google_crawler.py
@@ -6,11 +6,13 @@ from Spider import Spider
 
 def main():
     ### The start page's URL
-    start_url = 'https://scholar.google.com.tw/scholar?start=0&q=wdrc+algorithm&hl=zh-TW&as_sdt=0,5&as_vis=1'
+    start_url = 'https://scholar.google.com.tw/scholar?start=0&q=noise+reduction+algorithm&hl=zh-TW&as_sdt=0,5&as_ylo=2013'
 
     ### p_key and n
-    p_key = ['wdrc', 'dynamic range compression', 'hearing aid', 'speech']
-    n_key = ['imagery', 'image', 'visual', 'video']
+    p_key = ['wdrc', 'dynamic range compression', 'hearing aid', 'speech',
+             'noise cancellation', 'noise reduction']
+    n_key = ['imagery', 'image', 'visual', 'video', 'optic', 'opto', 'quantum',
+             'photon']
 
     ### Google Scholar Crawler, Class Spider
     myCrawler = Spider(start_url, p_key, n_key)
